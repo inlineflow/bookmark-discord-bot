638c13210613d54fc866a8f26422eff3c8e656ed
Eleina Hayden <eleina@member.fsf.org>
2025-11-09 10:38:54 -0600

    fix: make primary keys auto-increment

--- sql/schema/001_bookmarks.sql
+++ sql/schema/001_bookmarks.sql
@@ -1,18 +1,18 @@
 -- +goose Up
 CREATE TABLE guilds (
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     name TEXT UNIQUE NOT NULL,
     external_id INT UNIQUE NOT NULL
 );
 
 CREATE TABLE channels (
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     external_id INT UNIQUE NOT NULL
 );
 
 CREATE TABLE bookmarks (
-    id INT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     guild_id INT NOT NULL REFERENCES guilds(id),
     channel_id INT NOT NULL REFERENCES channels(id),
     author TEXT NOT NULL,
