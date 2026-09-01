f39bb36c937fefe05527d2063210f605ff829850
Eleina Mironia <ellie@eleina.dev>
2026-09-01 10:08:02 -0500

    Port to Disgo library (#1)
    * feat: port over to Disgo
    
    * fix: include event listener
    
    * fix: bad imports
    
    * fix: nil pointer refs, no app response

---
+++ internal/bot/handlers.go
@@ -0,0 +1,30 @@
+package bot
+
+import (
+	"log/slog"
+
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
+)
+
+func (b *Bot) bookmark(e *events.ApplicationCommandInteractionCreate, d discord.MessageCommandInteractionData) error {
+	msg := d.Resolved.Messages[d.TargetID()]
+	if &msg == nil {
+		return InvisibleReply("Could not find the message.", e)
+	}
+
+	dmChannel, err := b.Client.Rest().CreateDMChannel(e.User().ID)
+	if err != nil {
+		slog.Error("failed to create DM channel", "error", err)
+	}
+	msgChannel, err := b.Client.Rest().GetChannel(e.Channel().ID())
+	if err != nil {
+		slog.Error("failed to fetch channel", "error", err)
+	}
+	msgGuild, err := b.Client.Rest().GetGuild(*e.GuildID(), false)
+	if err != nil {
+		slog.Error("failed to fetch guild", "error", err)
+	}
+
+	return EmbedResponse(b, msg, dmChannel, msgChannel, msgGuild, e)
+}
